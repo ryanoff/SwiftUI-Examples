d9d70b125b451db403d37e5a407c16855c9fb5fd
Ryan Off <ryan@ryanoff.com>
2019-12-11 23:12:12 -0800

    Messing around with a TestView

--- SwiftUI Examples/VIews/TestView.swift
+++ SwiftUI Examples/VIews/TestView.swift
@@ -9,12 +9,17 @@
 import SwiftUI
 
 struct TestView: View {
+    @EnvironmentObject var appSettings: AppSettings
+    @EnvironmentObject var networkManager: NetworkManager
+
     var body: some View {
-        HStack {
-            Color.orange
-            Text("Hello, World!")
-            Color.red
-                .opacity(0.8)
+        VStack {
+            TextField("Username", text: $appSettings.username)
+            TextField("Password", text: $appSettings.password)
+            Button("Update"){
+                self.networkManager.mockService()
+            }
+            Divider()
             SecondView()
         }
 
@@ -22,13 +27,35 @@ struct TestView: View {
 }
 
 struct SecondView: View {
+    @EnvironmentObject var appSettings: AppSettings
+
     var body: some View {
-            Text("42")
+        VStack{
+            Text("\(appSettings.username)")
+            Text("\(appSettings.password)")
+        }
     }
+}
+
+class AppSettings: ObservableObject {
+    @Published var username: String = "luke@skywalker.com"
+    @Published var password: String = "r2d2c3po"
+}
+
+class NetworkManager: ObservableObject {
+//    @Binding var appSettings: AppSettings
+
+    @Published var token: String = ""
+
+    func mockService () {
+//        token = appSettings.username + appSettings.password
+    }
+    
 }
 
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
         TestView()
+        .environmentObject(AppSettings())
     }
 }
